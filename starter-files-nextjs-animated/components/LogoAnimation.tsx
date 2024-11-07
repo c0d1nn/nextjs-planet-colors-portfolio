adedc6afd4fb328c2cfd6e414543dbf27db252e8
Codin Zero <153553122+c0d1nn@users.noreply.github.com>
2024-11-07 13:27:35 +0100

    Update LogoAnimation.tsx

---
+++ starter-files-nextjs-animated/components/LogoAnimation.tsx
@@ -0,0 +1,34 @@
+// This line tells Next.js that this component uses client-side features
+"use client"
+
+// Import necessary components and libraries
+import Image from "next/image"
+import { motion } from "framer-motion"
+import udemy from "../assets/udemy.png"
+import fiverr from "../assets/fiverr.png"
+import elementor from "../assets/elementor.png"
+import logitech from "../assets/logitech.png"
+import oracle from "../assets/oracle.png"
+// Define an array of image objects, currently empty
+const images = [
+    { src: udemy, alt: "figma" },
+    { src: fiverr, alt: "fiverr" },
+    { src: elementor, alt: "elementor" },
+    { src: logitech, alt: "logitech" },
+    { src: oracle, alt: "oracle" },
+    { src: udemy, alt: "figma" },
+    { src: fiverr, alt: "fiverr" },
+    { src: elementor, alt: "elementor" },
+    { src: logitech, alt: "logitech" },
+    { src: oracle, alt: "oracle" },
+    { src: udemy, alt: "figma" },
+    { src: fiverr, alt: "fiverr" },
+    { src: elementor, alt: "elementor" },
+    { src: logitech, alt: "logitech" },
+    { src: oracle, alt: "oracle" },
+    { src: udemy, alt: "figma" },
+    { src: fiverr, alt: "fiverr" },
+    { src: elementor, alt: "elementor" },
+    { src: logitech, alt: "logitech" },
+    { src: oracle, alt: "oracle" },
+]
